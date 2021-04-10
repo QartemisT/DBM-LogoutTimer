ae92df92840eacca15936eece3b51640afa1140d
Artemis <QartemisT@gmail.com>
2021-04-10 04:29:34 +0100

    Update localization.en.lua

--- DBM-LogoutTimer/localization.en.lua
+++ DBM-LogoutTimer/localization.en.lua
@@ -8,6 +8,11 @@ L:SetTimerLocalization({
 	TimerLogout	= "Logout: "
 })
 
+L:SetOptionLocalization({
+	FlashClientIcon	= "Flash client icon when about to logout.",
+	PlaySound		= "Play sound effect when about to logout."
+})
+
 L:SetMiscLocalization({
 	IdleMessage		= "You are now Away: AFK",
 	UnidleMessage	= "You are no longer Away."
